a53bdc89db00ab579eea046cc757ee43c19c968b
Shahrier Akram <shahrier.akram@gmail.com>
2009-12-30 04:29:59 -0500

    Updated the tooltip library that works with IE

--- javascripts/jquery.tooltip.js
+++ javascripts/jquery.tooltip.js
@@ -1,3 +1,16 @@
+/**
+ * JQuery Tooltip Plugin
+ *
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ *
+ * Written by Shahrier Akram <shahrier.akram@gmail.com>
+ *
+ * Tooltip is a jQuery plugin implementing unobtrusive javascript interaction that appears
+ * near DOM elements, wherever they may be, that require extra information. 
+ *
+ * Visit http://gdakram.github.com/JQuery-Tooltip-Plugin for demo.
+**/
+
 (function($) {
 
    $.fn.tooltip = function(settings) {
@@ -34,7 +47,6 @@
        dialog_content = $(target_elm).find(config.dialog_content_selector)
        dialog_box = _create(dialog_content);
        
-       $("body").append(dialog_box);
        target_elm_position = $(target_elm).offset();
 
        // coming from the top right
@@ -122,19 +134,17 @@
      
      /**
       * Creates the dialog box element
+      * and appends it to the body
       **/
      function _create(content_elm) {
-       dialog_box = $("<div class='tooltip'>\
+       header = ($(content_elm).attr("title")) ? "<h1>" + $(content_elm).attr("title") + "</h1>" : '';
+       return $("<div class='tooltip'>\
          <div class='up_arrow arrow'></div>\
-         <div class='left_arrow arrow'></div\
-         <div class='content'></div>\
+         <div class='left_arrow arrow'></div>\
+         <div class='content'>" + header + $(content_elm).html() + "</div>\
          <div style='clear:both'></div>\
          <div class='down_arrow arrow'></div>\
-       </div>");
-       header = ($(content_elm).attr("title")) ? "<h1>" + $(content_elm).attr("title") + "</h1>" : '';
-       content = $(content_elm).html();
-       $(dialog_box).find("div.content").html(header + content)
-       return dialog_box;
+       </div>").appendTo('body');
      };
           
      return this; 
